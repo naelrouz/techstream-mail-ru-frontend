414a6eac4e381c3d1261295b51cdbe39a87a44cb
Ivan Novoselov <nael_rouz@mail.ru>
2018-02-12 20:28:37 +0300

    Работает логин и получение своих даных

---
+++ 2.2/client/index.js
@@ -0,0 +1,129 @@
+const auth = (username, email, cb) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/auth', true);
+  xhr.withCredentials = true;
+
+  const user = { username, email };
+  const body = JSON.stringify(user);
+
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=utf8');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState !== 4) {
+      return null;
+    }
+    if (xhr.status !== 200) {
+      return cb(xhr, null);
+    }
+
+    const res = JSON.parse(xhr.responseText);
+
+    cb(null, res);
+
+    return true;
+  };
+  xhr.send(body);
+};
+
+const whoami = cb => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', '/me', true);
+  xhr.withCredentials = true;
+
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=utf8');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState !== 4) {
+      return null;
+    }
+    if (xhr.status !== 200) {
+      return cb(xhr, null);
+    }
+
+    const res = JSON.parse(xhr.responseText);
+
+    cb(null, res);
+
+    return true;
+  };
+  xhr.send();
+};
+
+const manuItems = [
+  { sectionId: 'login', name: 'Вход' },
+  { sectionId: 'profile', name: 'Профайл' },
+  { sectionId: 'about', name: 'Об игре' }
+];
+
+const app = document.querySelector('#app');
+const nav = document.getElementById('navigation');
+
+manuItems.forEach(menuItem => {
+  const button = document.createElement('input');
+  button.classList.add('nav__item');
+  button.setAttribute('type', 'button');
+  button.setAttribute('data-section', menuItem.sectionId);
+  button.value = menuItem.name;
+  nav.appendChild(button);
+});
+
+// const navItems = document.querySelectorAll('.nav__item');
+
+const sectionsHTMLC = app.getElementsByTagName('section');
+
+//TODO const
+
+// console.log(sectionsHTMLC);
+
+nav.addEventListener('click', event => {
+  // e.preventDefault();
+
+  const { target } = event;
+
+  Array.from(sectionsHTMLC).forEach(sectionsItem => {
+    const section = sectionsItem;
+    section.hidden = true;
+    return null;
+  });
+
+  target.style.color = 'red';
+  const sectionId = target.getAttribute('data-section');
+
+  if (sectionId === 'profile') {
+    whoami((err, res) => {
+      if (err) {
+        console.log('err: ', err);
+        return null;
+      }
+      console.log('res: ', res);
+
+      return null;
+    });
+  }
+
+  const section = document.getElementById(sectionId);
+
+  section.hidden = false;
+
+  // console.log(e);
+});
+
+// sections.reduce(section => {});
+
+const loginForm = document.querySelector('.login_form');
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const form = event.target;
+
+  const email = form.elements.email.value;
+
+  const username = form.elements.username.value;
+
+  auth(username, email, (err, res) => {
+    if (err) {
+      // alert(`AUTH Error: ${err.status}`);
+      console.log('err: ', err);
+      return null;
+    }
+    form.reset();
+  });
+});
---
+++ 2.2/server/server.js
@@ -0,0 +1,88 @@
+const Koa = require('koa');
+const koaStatic = require('koa-static');
+const koaLogger = require('koa-logger');
+const uuidv4 = require('uuid/v4');
+
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(koaStatic('../client'));
+app.use(koaLogger());
+app.use(
+  koaBody({
+    multipart: true
+  })
+);
+
+const Router = require('koa-router');
+
+const router = new Router();
+
+const users = {};
+const ids = {};
+
+router.post('/auth', async ctx => {
+  const { username, email } = ctx.request.body;
+
+  console.log(username);
+
+  if (!username || !email) {
+    ctx.status = 400;
+  }
+
+  if (!users[email]) {
+    users[email] = {
+      username,
+      email,
+      count: 0
+    };
+  }
+
+  const id = uuidv4();
+  ids[id] = email;
+
+  ctx.cookies.set('podvorot', id, {
+    domain: 'localhost',
+    expires: new Date(Date.now() + 1000 * 60 * 10)
+  });
+
+  console.log('users: ', users);
+  console.log('ids: ', ids);
+
+  ctx.body = { id };
+});
+
+router.get('/me', async ctx => {
+  const id = ctx.cookies.get('podvorot');
+  const email = ids[id];
+
+  console.log('"/me" id: ', id);
+  console.log('"/me" email: ', email);
+  console.log('"/me" users[email]: ', users[email]);
+
+  if (!email || !users[email]) {
+    ctx.status = 401;
+  }
+
+  users[email].count += 1;
+
+  ctx.body = users[email];
+});
+
+router.get('/views', async ctx => {
+  const n = ctx.cookies.get('view') + 1;
+  ctx.cookies.set(`view ${n}`);
+  ctx.body = `${n} views`;
+});
+
+// router.get('/views', async (ctx, next) => {
+//   let count = ctx.session.count || 0;
+//   ctx.session.count = ++count;
+
+//   ctx.body = count;
+// });
+
+app.use(router.routes());
+
+module.exports = app;
